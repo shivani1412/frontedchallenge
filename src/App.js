37e1c9993800aa363d94fad6501158fd2960fb3a
shivani <jshivani1412@gmail.com>
2022-10-20 13:34:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import GithubUsers from "./components/githubUsers";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [users, setUsers] = useState([]);
+
+    const getUsers = async () => {
+        
+            const response = await fetch( "https://api.github.com/users");
+            setUsers(await response.json());
+        
+    }
+
+    useEffect(() => {
+        getUsers();
+    },);
+
+    
+    return (
+        <>
+            <GithubUsers users={users}/>
+        </>
+    )
 }
 
 export default App;
